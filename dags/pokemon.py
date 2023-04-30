0e8d65106b5824ebd9c19c19707a75753feb7b4b
Djalma Henrique <djalmahenry@outlook.com>
2023-04-29 21:34:55 -0300

    feat: adding new dag

--- dags/pokemon.py
+++ dags/pokemon.py
@@ -1,42 +1,37 @@
 import requests
 import json
+from google.cloud import storage
 
-url = "https://pokeapi.co/api/v2/pokemon/"
-pokemon_list = list()
 
-counter = 1
+def extract_pokemon():
 
-while url != None:
-    payload={}
-    headers = {}
-    response = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
-    url = response["next"]
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    pokemon_list = {'pokemon_list': list()}
 
-    for item in response["results"]:
-        pokemon_name = item["name"]
-        url_pokemon = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
-        response_pokemon = json.loads(requests.request("GET", url_pokemon, headers=headers, data=payload).text)
+    while url != None:
+        payload={}
+        headers = {}
+        response = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+        url = response["next"]
 
-        infos = {
-            "name": pokemon_name,
-            "id": response_pokemon["id"],
-            "height": response_pokemon["height"],
-            "weight": response_pokemon["weight"],
-            "is_default": response_pokemon["is_default"]
-        }
+        for item in response["results"]:
+            pokemon_name = item["name"]
+            url_pokemon = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+            response_pokemon = json.loads(requests.request("GET", url_pokemon, headers=headers, data=payload).text)
 
-        pokemon_list.append(infos)
-        print(response_pokemon["id"])
+            infos = {
+                "name": pokemon_name,
+                "id": response_pokemon["id"],
+                "height": response_pokemon["height"],
+                "weight": response_pokemon["weight"],
+                "is_default": response_pokemon["is_default"]
+            }
 
-    file_path = fr"/Users/djalmahenrique/Documents/GitHub/airflow-data-engineering/pokemon_files/pokemon_file_{counter}.json"
+            pokemon_list['pokemon_list'].append(infos)
+            print(response_pokemon["id"])
 
-    with open(file_path, 'w') as outfile:
-        print("Salvando arquivo em: ", file_path)
-        json.dump(pokemon_list, outfile)
-
-    outfile.close()
-    counter = counter + 1
-    pokemon_list = list()
-
-print(pokemon_list)
-
+    bucket_name = "airflow-pipelines"
+    bucket = storage.Client().get_bucket(bucket_name)
+    blob = bucket.blob("pokemon_file.json")
+    print(f"Salvando arquivo em: {bucket_name}")
+    blob.upload_from_string(data=json.dumps(pokemon_list), content_type='application/json')  
---
+++ dags/pokemon_extraction.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from datetime import datetime
+from pokemon import extract_pokemon
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+
+default_args = {
+    "owner": "djalmahenry",
+    "start_date": datetime(2023, 4, 29),
+}
+
+dag = DAG(
+    "pokemon_extraction",
+    default_args=default_args,
+    schedule_interval="20 09 * * 2",
+    max_active_runs=1,
+)
+
+start_pipeline = DummyOperator(
+    task_id="start_pipeline",
+    dag=dag
+)
+
+extract_pokemons = PythonOperator(
+    task_id='extract_pokemons',
+    python_callable=extract_pokemon,
+    dag=dag
+)
+
+done_pipeline = DummyOperator(
+    task_id="done_pipeline",
+    dag=dag
+)
+
+start_pipeline >> extract_pokemons >> done_pipeline
